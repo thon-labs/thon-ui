398e9112f82a04e36cdebdb09c2a880409242b9b
Gustavo Sales <gustavo@gsales.io>
2022-10-18 16:05:40 -0300

    feat: add themes to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,14 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    'storybook-addon-themes',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        backgrounds: false,
+      },
+    },
     {
       name: '@storybook/addon-postcss',
       options: {
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,12 @@
 import '../src/core/fonts.css';
 import '../src/core/base.css';
+import './storybook.css';
+
+const systemIsDarkMode =
+  window.matchMedia &&
+  window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+localStorage.setItem('theme', systemIsDarkMode ? 'dark' : 'light');
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -7,6 +14,17 @@ export const parameters = {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
+    },
+  },
+  themes: {
+    default: systemIsDarkMode ? 'Dark' : 'Light',
+    list: [
+      { name: 'Light', color: '#f3f4fa' },
+      { name: 'Dark', class: 'dark', color: '#1c1c1c' },
+      { name: 'White', class: 'white', color: '#ffffff' },
+    ],
+    onChange: ({ name }) => {
+      localStorage.setItem('theme', name.toLowerCase());
     },
   },
 };
