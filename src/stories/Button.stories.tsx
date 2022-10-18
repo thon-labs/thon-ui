da94f191c9f17e5bdd3bb99ca22a18cf9e0a810d
Gustavo Sales <gustavo@gsales.io>
2022-10-18 17:15:21 -0300

    feat: update button stories

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -9,6 +9,7 @@ export default {
 } as ComponentMeta<typeof Button>;
 
 const commonProps = {
+  children: 'My Button',
   size: 'medium',
   as: 'button',
   loading: false,
@@ -21,27 +22,23 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   variant: 'primary',
-  children: 'My Primary Button',
   ...commonProps,
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
   variant: 'secondary',
-  children: 'My Secondary Button',
   ...commonProps,
 };
 
 export const Ghost = Template.bind({});
 Ghost.args = {
   variant: 'ghost',
-  children: 'My Ghost Button',
   ...commonProps,
 };
 
 export const Danger = Template.bind({});
 Danger.args = {
   variant: 'danger',
-  children: 'My Delete Button',
   ...commonProps,
 };
